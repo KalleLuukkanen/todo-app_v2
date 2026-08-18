9c9d43e9356e479c52b2a72cae967448889111e7
Kalle Luukkanen <kalleluukkanen@gmail.com>
2026-08-18 12:50:21 +0300

    setting up the client

---
+++ client/src/components/pages/Home.tsx
@@ -0,0 +1,7 @@
+function Home() {
+    return (
+        <div>homepage</div>
+    )
+}
+
+export default Home
---
+++ client/src/components/pages/about/About.tsx
@@ -0,0 +1,7 @@
+function About() {
+    return (
+        <div>aboutpage</div>
+    )
+}
+
+export default About
